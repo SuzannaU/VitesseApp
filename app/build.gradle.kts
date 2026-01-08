a07190f064ef339f6fae17df50dace50fb89719e
SuzannaU <suzanna.ursulet@gmail.com>
2026-01-08 14:10:02 +0100

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,103 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.room)
+}
+
+android {
+    namespace = "com.openclassrooms.vitesseapp"
+    compileSdk {
+        version = release(36)
+    }
+
+    defaultConfig {
+        applicationId = "com.openclassrooms.vitesseapp"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    room {
+        schemaDirectory("$projectDir/schemas")
+    }
+
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_11
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+ksp {
+    arg("Room.incremental", "true")
+    arg("Room.generateKotlin", "true")
+}
+
+dependencies {
+    implementation(libs.androidx.appcompat)
+
+    // Kotlin
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.coroutines.core)
+    implementation(libs.coroutines.android)
+    testImplementation(libs.coroutines.test)
+
+    // Koin
+    implementation(platform(libs.koin.bom))
+    implementation(libs.koin.core)
+    implementation(libs.koin.android)
+    testImplementation(libs.koin.test)
+    testImplementation(libs.koin.test.junit5)
+
+    // Room
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    ksp(libs.room.compiler)
+
+    // Retrofit - Moshi
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter)
+    implementation(libs.moshi)
+    ksp(libs.moshi.codegen)
+
+    // Coil
+    implementation(libs.coil)
+    implementation(libs.coil.network)
+
+    // UI
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+
+    // Testing
+    testImplementation(libs.junit)
+    testImplementation(libs.mockk)
+    testImplementation(libs.turbine)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
