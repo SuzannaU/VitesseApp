b9a76e3b48eebe7688c164c17ba7063a9168aba7
SuzannaU <suzanna.ursulet@gmail.com>
2026-02-12 17:45:50 +0100

    Addition of Equals and hashcode overrides in CandidateDTO

--- app/src/main/java/com/openclassrooms/vitesseapp/data/entity/CandidateDto.kt
+++ app/src/main/java/com/openclassrooms/vitesseapp/data/entity/CandidateDto.kt
@@ -37,4 +37,38 @@ data class CandidateDto(
 
     @ColumnInfo(name = "is_favorite")
     var isFavorite: Boolean = false,
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as CandidateDto
+
+        if (candidateId != other.candidateId) return false
+        if (birthdate != other.birthdate) return false
+        if (salaryCentsInEur != other.salaryCentsInEur) return false
+        if (isFavorite != other.isFavorite) return false
+        if (firstname != other.firstname) return false
+        if (lastname != other.lastname) return false
+        if (!photoByteArray.contentEquals(other.photoByteArray)) return false
+        if (phone != other.phone) return false
+        if (email != other.email) return false
+        if (notes != other.notes) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = candidateId.hashCode()
+        result = 31 * result + birthdate.hashCode()
+        result = 31 * result + (salaryCentsInEur?.hashCode() ?: 0)
+        result = 31 * result + isFavorite.hashCode()
+        result = 31 * result + firstname.hashCode()
+        result = 31 * result + lastname.hashCode()
+        result = 31 * result + (photoByteArray?.contentHashCode() ?: 0)
+        result = 31 * result + phone.hashCode()
+        result = 31 * result + email.hashCode()
+        result = 31 * result + (notes?.hashCode() ?: 0)
+        return result
+    }
+}
